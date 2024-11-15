563b9c31ef503193260143d6488121fe09e5cc31
mateuszmarc <mmarckiewicz@gmail.com>
2024-11-15 12:44:46 +0000

    Create Spring Boot Project

---
+++ src/main/java/com/marcykiewicz/mateusz/joba_application_portal/JobaApplicationPortalApplication.java
@@ -0,0 +1,13 @@
+package com.marcykiewicz.mateusz.joba_application_portal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JobaApplicationPortalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JobaApplicationPortalApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/marcykiewicz/mateusz/joba_application_portal/JobaApplicationPortalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.marcykiewicz.mateusz.joba_application_portal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JobaApplicationPortalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
