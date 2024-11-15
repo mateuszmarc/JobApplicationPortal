56384140587f73da7ba9ac248962dbddc86836ac
mateuszmarc <mmarckiewicz@gmail.com>
2024-11-15 14:16:43 +0000

    Add needed dependencies to pom.xml file. Create HomeController Add datasource connection properties in properties file Add static resources Add index.html and registration.html views

---
+++ src/main/java/com/marcykiewicz/mateusz/joba_application_portal/controller/HomeController.java
@@ -0,0 +1,13 @@
+package com.marcykiewicz.mateusz.joba_application_portal.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HomeController {
+
+    @GetMapping("/")
+    public String home() {
+        return "index";
+    }
+}
