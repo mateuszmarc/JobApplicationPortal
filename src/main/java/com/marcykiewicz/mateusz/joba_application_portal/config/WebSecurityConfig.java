68ba9f4f2d6f66ae7c522a1d05a6c9f5e21a0561
mateuszmarc <mmarckiewicz@gmail.com>
2024-11-18 03:52:25 +0000

    Configure custom web security configuration: - Create CustomUserDetailsService - Create CustomUserDetails Configure WebSecurityConfig to use CustomUserDetailsService and CustomUserDetails

--- src/main/java/com/marcykiewicz/mateusz/joba_application_portal/config/WebSecurityConfig.java
+++ src/main/java/com/marcykiewicz/mateusz/joba_application_portal/config/WebSecurityConfig.java
@@ -1,10 +1,17 @@
 package com.marcykiewicz.mateusz.joba_application_portal.config;
 
+import com.marcykiewicz.mateusz.joba_application_portal.service.CustomUserDetailsService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
+@RequiredArgsConstructor
 @Configuration
 public class WebSecurityConfig {
 
@@ -28,9 +35,13 @@ public class WebSecurityConfig {
             "/error"
     };
 
+    private final CustomUserDetailsService customUserDetailsService;
+
 
     @Bean
-    protected SecurityFilterChain securityFilterChain(HttpSecurity security) throws Exception {
+    public SecurityFilterChain securityFilterChain(HttpSecurity security) throws Exception {
+
+        security.authenticationProvider(authenticationProvider());
 
         security.authorizeHttpRequests(auth -> {
                     auth.requestMatchers(publicUrl).permitAll();
@@ -41,5 +52,19 @@ public class WebSecurityConfig {
 
 
         return security.build();
+    }
+
+    @Bean
+    public AuthenticationProvider authenticationProvider() {
+
+        DaoAuthenticationProvider daoAuthenticationProvider = new DaoAuthenticationProvider();
+        daoAuthenticationProvider.setPasswordEncoder(passwordEncoder());
+        daoAuthenticationProvider.setUserDetailsService(customUserDetailsService);
+        return daoAuthenticationProvider;
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
     }
 }
