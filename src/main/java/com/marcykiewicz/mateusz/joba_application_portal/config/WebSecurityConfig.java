7e0cae12f239c6b9a82c725a16c465dc7e67040a
mateuszmarc <mmarckiewicz@gmail.com>
2024-11-19 16:49:13 +0000

    Implement functionality to enable JobPostActivity edition

--- src/main/java/com/marcykiewicz/mateusz/joba_application_portal/config/WebSecurityConfig.java
+++ src/main/java/com/marcykiewicz/mateusz/joba_application_portal/config/WebSecurityConfig.java
@@ -48,7 +48,11 @@ public class WebSecurityConfig {
 
         security.authorizeHttpRequests(auth -> {
                     auth.requestMatchers(publicUrl).permitAll();
-                    auth.requestMatchers("/recruiter-profile/**", "/jobs/new-job/**").hasAuthority("Recruiter");
+                    auth.requestMatchers("" +
+                            "/recruiter-profile/**",
+                            "/jobs/new-job/**",
+                            "/jobs/edit/**"
+                            ).hasAuthority("Recruiter");
                     auth.anyRequest().authenticated();
                 }
         );
